91805c36febc9bda178246c101bd40afbd1890e5
Dipo <31691434+Dipo-Jaiye@users.noreply.github.com>
2022-03-20 04:39:00 +0100

    Delete instance directory

--- instance/config.py
+++
@@ -1,9 +0,0 @@
-TESTCONFIG='mysecret'
-MERCHANT_ID = 'GlorybetoGod'
-DEBUG=True
-ADMIN_EMAIL=''
-FLASK_RUN_PORT=5000
-SECRET_KEY='guessmysecretkeyhaha'
-
-SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://dipo:d!poPA##c043@localhost/walletstuff'
-SQLALCHEMY_TRACK_MODIFICATIONS=False
