e642f95bfab6a3983dd64ea4b38be9760d226efb
Oladipupo Durojaiye <durojaiyep@yahoo.co.uk>
2022-03-20 04:47:15 +0100

    deploy branch

---
+++ instance/config.py
@@ -0,0 +1,9 @@
+TESTCONFIG='mysecret'
+MERCHANT_ID = 'GlorybetoGod'
+DEBUG=True
+ADMIN_EMAIL=''
+FLASK_RUN_PORT=5000
+SECRET_KEY='guessmysecretkeyhaha'
+
+SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://9iApSsaija:y7ntwdt1Yt@remotemysql.com:3306/9iApSsaija'
+SQLALCHEMY_TRACK_MODIFICATIONS=False
